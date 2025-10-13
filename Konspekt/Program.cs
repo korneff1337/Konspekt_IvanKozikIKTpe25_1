079d86687ccc38a999e30088f626025dc96cf632
korneff1337 <74781679+korneff1337@users.noreply.github.com>
2025-10-13 10:22:20 +0300

    operaatorid ja tsüklid

--- Konspekt/Program.cs
+++ Konspekt/Program.cs
@@ -489,3 +489,90 @@ else if (ostusumma < 1)
     // mis asub funktsiooni taga olevate sulgude vahel.
 
 } // peale tingimust on koodiplokk {} loogeliste sulgude vahel. koodiplokk sisesldab endas ühte rida, kus muutujasse tulemus omistatakse nüüd väärtus liites kokku
+
+//string kasutajaNimi = "";
+//do
+//{
+//    Console.WriteLine("Palun sisesta oma kasutajanimi");
+//    kasutajaNimi = Console.ReadLine();
+//}
+//while (kasutajaNimi != "user1");
+//if (kasutajaNimi == "user1")
+//{
+//    int ruuduSuurus = 0;
+
+//    Console.WriteLine("Kui suurt ruutu tahad?");
+
+//    do
+//    {
+//        Console.WriteLine("Kui suurt ruutu tahad?");
+//        ruuduSuurus = int.Parse(Console.ReadLine());
+//    } while (ruuduSuurus < 0 && ruuduSuurus > 20);
+//    char reaKujund = '█';
+//    string üksRida = "";
+//    int tsükliMuutuja = ruuduSuurus;
+//    do
+//    {
+//        üksRida += "_" + reaKujund;
+//        tsükliMuutuja -= 1;
+//    } while (tsükliMuutuja != 0);
+//    tsükliMuutuja = ruuduSuurus;
+//    do
+//    {
+//        Console.WriteLine(üksRida);
+//        tsükliMuutuja -= 1;
+
+//    } while (tsükliMuutuja != 0);
+//    Console.WriteLine($"Palun, siin on sinu ruut, suurusega {ruuduSuurus}x{ruuduSuurus}");
+//}
+
+/* tingimuslause osad */
+if (true) { } //kaitstud sõna if kutsub esile tingimuslause, mille tingimus on sulgude vahel ning millele järgneb
+              //koodiplok tingimuse täitumisel teostatava koodiga
+else if (true) { } //kaitstud sõnad else ja if (else if) kutsuvad esile sekundaarse tingimuslause, mille tingimus
+                   //on samamoodi sulgude ahel, ning millele peab eelnema alati if või else if. Tingimuse täitumisel
+                   //ja eelneva tingimuse mittetäitumisel teostaakse koodiploki sees olev koode
+else { } //kaitstud sõna else kutsub esile järeltingimuse, millele peab eelnema kas if või else if, ning mille koodiploki sisu
+         //täidetakse kõikide if ja else if sees olevate tingimuste läbikukkumisel.
+
+/* Loogilised tehted */
+//&& -> "and" loogiline tehe, mida kasutatakse tingimuste kirjutamisel, ning mis annab positiivse vastuse (true) juhul kui
+//      mõlemal pool && märki olevad tingimused on täidetud. Kui üks neist ei ole, siis annab negatiivse vastuse (false).
+// || -> "or" loogiline tehe, mida kasutatakse tingimuste kirjutamisel, ning mis annab positiivse vastuse (true) siis kui
+//      vähemalt üks tingimus on täidetud. negatiivne vastus (false) tagastatakse siis, kui kõik tingimused on täitmata.
+// !  -> "not" loogiline tehe, mida kasutatakse tingimuse tulemuse inverteerimiseks. Tulemus mis muidu tagastaks true, hüüumärgi
+//       puhul tagastab false, ja vastupidi - tulemus mis muidu tagastaks false, hüüumärgi puhul tagastab true.
+
+/* Võrdlusoperaatorid */
+// == -> on võrdne. Võrdusmärkide ühel pool olev objekt peab vastama täpselt oma olemuselt mõrdusmärgi teisel pool oleva
+// objektiga. ei ole sama nagu üks võrdusmärk.  üks võrdusmärk omistb, kaks võrdleb.
+// != -> ei ole võrdne. Võrdusmärgi ühel pool olev objekt *EI TOHI* olla samal kujul nagu teisel pool olev objekt.
+//       Võrdlus operaator on kombinatsioon "on võrdne" operaatorist, ja loogilisest tehtest "not".
+// >  -> on suurem kui. Märgist vasakul pool olev objektpeaks olema suurem kui paremal pool olev objekt.
+// <  -> on väiksem kui. Märgist vasakul pool olev objekt peaks olema väiksem, kui paremal pool olev objekt.
+// >= -> on suurem kui või võrdne kui. Märgist vasakul pool olev objekt peaks olema vähemalt võrdne või suurem
+//       kui paremal pool olev objekt. Operaator on kombinatsioon "on võrdne" ja "on suurem kui" operaatoritest.
+// <= -> on väiksem kui või võrdne kui. Märgist vasakul pool olev objekt peaks olema vähemalt võrdne või väiksem
+//       kui paremal pool olev objekt. Operaator on kombinatsioon "on võrdne" ja "on väiksem kui" operaatoritest.
+
+/* Omistusoperaatorid ja kiirtehted */
+int arv = 1;// =  -> üksik võrdusmärk omistab muutujale väärtuse.
+arv += 1;   // += -> võrdusmärk mille ees on pluss, automaatselt liidab muutujale otsa võrdusmärgi teisel pool oleva arvu.
+            //       asendab tehet "arv = arv + 1" on kombinatsiooon matemaatilisest tehtest "+" ja omistamisest ("=")
+arv -= 1;   // -= -> võrdusmärk mille ees on miinus, automaatselt lahutab muutuja maha võrdusmärgi teisel pool oleva väärtuse.
+            //       asendab tehet "arv = arv - 1" on kombinatsioon matemaatilisest tehtest "-" ja omistamisest ("=")
+arv *= 2;   // *= -> võrdusmärk mille ees on korrutusmärk *. automaatselt korrutab muutuja sisu ja võrdusmärgi teisel pool oleva
+            //       arvu kordi. asendab tehet "arv = arv * 2". on kombinatsioon matemaatilisest tehtest "*" ja omistamisest
+            //       ("=")
+arv /= 2;   // *= -> võrdusmärk mille ees on jagamismärk /. automaatselt jagab muutuja sisu võrdusmärgi teisel pool oleva
+            //       arvu osadeks. asendab tehet "arv = arv / 2". on kombinatsioon matemaatilisest tehtest "/" ja omistamisest
+            //       ("=")
+arv++;     // ++ -> on spetsiifiliselt ühe juurde liitmiseks lühivariant.
+arv--;      // -- -> on spetsiifiliselt ühe maha lahutamiseks lühivariant.
+
+/* Tsüklid */
+// 1. do-while
+do // "do" on kaitstud sõna, mis alustab do-while tsüklit pärast mida on koodiplokk, ning ütleb et tee seda koodi
+{
+
+} while (true); //niikaua kuni while järel olevate sulgude vahel olev tingimus on täidetud.
