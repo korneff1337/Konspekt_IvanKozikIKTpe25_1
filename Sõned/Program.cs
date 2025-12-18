5d7b56f6f80e94ab3cae118ef55ab37f83de4242
Opilane TTHK <>
2025-12-18 09:27:05 +0200

    Sõned

---
+++ Sõned/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sõned
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lause = "tere kuidas läheb?";
+            ////Console.WriteLine("lause pikkus on: " +lause.Length);
+            ////Console.WriteLine("See tekst on capslockis: " + lause.ToUpper());
+            ////Console.WriteLine("See tekst on väiketäthtedes: " + lause.ToLower());
+            ////Console.WriteLine("Kas lasuses on sõna \"Tere\"?:" + lause.ToLower().Contains("Tere"));
+
+
+            //List<string> list = new List<string>() { "auto", "kraav", "kruus" };
+            //string listkoos = "";
+            //foreach (var sõne in list)
+            //{
+            //    listkoos += (" "+sõne.ToUpper()+",");
+            //}
+            //Console.WriteLine(listkoos);
+
+            //string eesnimi = "Julius";
+            //string perekonnanimi = "Caesar";
+            //string amet = "Ohver";
+            //string kuuluskeiser = string.Concat(eesnimi, perekonnanimi, amet);
+            //Console.WriteLine(kuuluskeiser);
+
+            //string kuuluskeiser2 = $"{eesnimi}, {perekonnanimi}, {amet}.";
+            //Console.WriteLine($"{eesnimi}, {perekonnanimi}, {amet}.");
+            //Console.WriteLine(kuuluskeiser2);
+
+            //string[] sõnadlauses = lause.Split(' ');
+            //foreach (var sõne in sõnadlauses)
+            //{
+            //    Console.WriteLine(sõne);
+            //}
+            //string ükssõna = "banan";
+            //Console.WriteLine(ükssõna[4]);
+            //Console.WriteLine(ükssõna.IndexOf("i"));
+
+            //Console.WriteLine(" \" ");
+
+            //Console.WriteLine("a\nb");
+
+            //Console.WriteLine("AAAA\tBBBB");
+        }
+    }
+}
